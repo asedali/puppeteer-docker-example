ae3d4a31f6fa6821dc3203b9d952ced36d4a069c
aSedali <toseyyedali@gmail.com>
2020-03-09 10:06:52 +0330

    Update app.js

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const express = require('express');
 const puppeteer = require('puppeteer');
-
-const app = express();
-
-app.get('/screenshot', async (req, res) => {
-  const browser = await puppeteer.launch({
-    executablePath: 'google-chrome-unstable',
-    args: ['--disable-gpu', '--no-sandbox', '--single-process',  '--disable-web-security']
-  });
-
+console.log(" starting...");
+(async () => {
+      const browser = await puppeteer.launch({
+        args: ['--no-sandbox']
+      });
+      
   const page = await browser.newPage();
+  console.log("go to ncript ...");
+          
+    //  page.setDefaultNavigationTimeout(0);
+      //page.setDefaultTimeout(0);
 
-  await page.setJavaScriptEnabled(true);
-  await page.goto(req.query.url);
+  await page.goto('https://mindmindi.blogspot.com/');
+  console.log("on new ...");
+  const innerText = await page.evaluate(() => document.querySelector('p').innerText);
+  console.log(innerText);
+            await page.waitFor(100000000);
+            console.log("out of new ...");
 
-  const screenshot = await page.screenshot({ type: 'jpeg', quality: 95 });
-  await browser.close();
-
-  res.end(screenshot);
-})
-
-app.listen(4000, () => console.log('listening on port 4000!'));
+ // await page.waitFor(1000000000);
+  
+})();
